0f62e91e70f56d8fdcda796daae213c04a76ba9a
rieger::CLOUD <riegercloud@users.noreply.github.com>
2018-04-20 15:33:39 +0200

    Update ssl-certificate.sh

--- ssl-certificate.sh
+++ ssl-certificate.sh
@@ -35,39 +35,39 @@ echo "Verify that both ports (80 + 443) are forwarded to this server!"
 echo "And verify, your dyndns points to your IP either!"
 echo "Then retry..."
 }
-#add the letsencrypt repository to the server
+###add the letsencrypt repository to the server
 add-apt-repository ppa:certbot/certbot -y
 update_and_clean
-#install letsencrypt
+###install letsencrypt
 apt install letsencrypt -y
-#ensure lower cases
+###ensure lower cases
 declare -l DYNDNSNAME
 declare -l YOURSERVERNAME
-#read the current hostname
+###read the current hostname
 YOURSERVERNAME=$(hostname)
-#ask the user what would be the domain name to request the certificate for
+###ask the user what would be the domain name to request the certificate for
 read -p "Your domain: " DYNDNSNAME
-#request the certificate for the entered domain name
+###request the certificate for the entered domain name
 letsencrypt certonly -a webroot --webroot-path=/var/www/letsencrypt --rsa-key-size 4096 -d $DYNDNSNAME
-#in case of any errors
+###in case of any errors
 if [ ! -d "/etc/letsencrypt/live" ]; then
 errorSSL
-#if no error appeared
+###if no error appeared
 else
 copy4SSL
-#remove the entry for the self signed certificates
+###remove the entry for the self signed certificates
 sed -i '/ssl-cert-snakeoil/d' /etc/nginx/ssl.conf
-#apply the new server/domain name to the NGINX vhost for Nextcloud
+###apply the new server/domain name to the NGINX vhost for Nextcloud
 sed -i "s/server_name.*;/server_name $DYNDNSNAME;/" /etc/nginx/conf.d/nextcloud.conf
-#change the placeholder values to the exact letsencrypt ones
+###change the placeholder values to the exact letsencrypt ones
 sed -in 's/YOUR.DEDYN.IO/'$DYNDNSNAME'/' /etc/nginx/ssl.conf
-# remove the '#' to enable these certificates/keys
+###remove the '#' to enable these certificates/keys
 sed -i s/\#\ssl/\ssl/g /etc/nginx/ssl.conf
-#ensure that dhparam is commented as long it was not generated 
+###ensure that dhparam is commented as long it was not generated 
 sed -i s/ssl_dhparam/\#ssl_dhparam/g /etc/nginx/ssl.conf
-#adjust Nextclous config.php to the new domain name
+###adjust Nextclous config.php to the new domain name
 sudo -u www-data sed -in 's/'$YOURSERVERNAME'/'$DYNDNSNAME'/' /var/www/nextcloud/config/config.php
-#restart the cloud environment
+###restart the cloud environment
 restart_all_services
 clear
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++"
